5129c3bf8f0e48c97e216fa0c0f7d916360eb0fa
Ezzio Moreira <enzziom@gmail.com>
2021-11-21 21:04:05 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ data "aws_ami" "ubuntu" {
 
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.nano"
+  instance_type = "t2.micro"
 
   tags = {
     Name = "HelloWorld"
