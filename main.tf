b249d964fea1c521d32b157024721cbe6707c04c
EzzioMoreira <enzziom@gmail.com>
2021-11-25 13:21:32 -0300

    add cred provider

--- main.tf
+++ main.tf
@@ -7,6 +7,8 @@ terraform {
     bucket = "metal.corp-devops-test"
     key    = "infra/atlantis-terraform-.tfstate"
     region = "us-east-2"
+    access_key = "AKIAXSFIMTBMQN2RFD7R"
+    secret_key = "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"
   }
 } 
 
