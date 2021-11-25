d718cf3852e2ab0834c27a009781ee6ee239ac2a
EzzioMoreira <enzziom@gmail.com>
2021-11-25 13:23:33 -0300

    teste

--- main.tf
+++ main.tf
@@ -6,14 +6,14 @@ terraform {
   backend "s3" {
     bucket = "metal.corp-devops-test"
     key    = "infra/atlantis-terraform-.tfstate"
-    region = "us-east-2"
+    region = "us-west-2"
     access_key = "AKIAXSFIMTBMQN2RFD7R"
     secret_key = "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"
   }
 } 
 
 resource "aws_s3_bucket" "b" {
-  bucket = "ezzio-my-tf-bucket"
+  bucket = "ezzio123-my-tf-bucket"
   acl    = "private"
 
   tags = {
