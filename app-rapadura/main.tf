a3c1d690f22ca4006d5b6802bb4f2a2d0d6a3978
Ezzio Moreira <enzziom@gmail.com>
2022-08-02 20:42:38 -0300

    feat: add app tf

---
+++ app-rapadura/main.tf
@@ -0,0 +1,20 @@
+resource "aws_s3_bucket" "this" {
+  for_each = local.context[terraform.workspace].buckets_name
+  bucket = each.value.bucket_name
+  acl    = "private"
+  tags   = local.tags
+
+  versioning {
+    enabled    = true
+    mfa_delete = false
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "this" {
+  for_each                = local.context[terraform.workspace].buckets
+  bucket                  = aws_s3_bucket.this[each.key].id
+  block_public_acls       = true
+  block_public_policy     = true
+  restrict_public_buckets = true
+  ignore_public_acls      = true
+}
